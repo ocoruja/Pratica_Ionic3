b8d33d3bf7167401d723096a144c13d2956d70cf
ocoruja <tgribeiro@gmail.com>
2017-08-21 21:10:00 -0300

    Cálculo dos acessórios

--- src/pages/escolha/escolha.ts
+++ src/pages/escolha/escolha.ts
@@ -8,14 +8,26 @@ export class EscolhaPage {
 
     public carro;
     public acessorios;
+    private _precoTotal:number;
 
     constructor(public navParams: NavParams) {
 
         this.carro = navParams.get('carroSelecionado');
+        this._precoTotal = this.carro.preco;
         this.acessorios = [
             { nome: 'Freio ABS', preco: 800 },
             { nome: 'Ar-condicionado', preco: 1000 },
             { nome: 'MP3 Player', preco: 500}
         ];
+    }
+
+    get precoTotal() {
+        return this._precoTotal;
+    }
+
+    atualizaTotal(ligado: boolean, acessorio) {
+        ligado ?
+            this._precoTotal+= acessorio.preco :
+            this._precoTotal-= acessorio.preco;
     }
 }
