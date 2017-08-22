b68aea60e1b7468b8224cb4141c913693033da08
ocoruja <tgribeiro@gmail.com>
2017-08-22 17:02:01 -0300

    Ativando o botão de avançar no agendamento

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Carro } from '../../domain/carro/carro';
 
 @IonicPage()
 @Component({
@@ -8,7 +8,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CadastroPage {
 
+  public carro: Carro;
+  public precoTotal: number;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.carro = this.navParams.get('carro');
+    this.precoTotal = this.navParams.get('precoTotal')
   }
 
 }
