82672822fec58717b4616d37ff5b902ed4fb7fdb
ocoruja <tgribeiro@gmail.com>
2017-08-24 09:45:24 -0300

    Inserindo formulários na pagina de cadastro

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -10,9 +10,21 @@ export class CadastroPage {
 
   public carro: Carro;
   public precoTotal: number;
+
+  public nome: string;
+  public endereco: string;
+  public email: string;
+  public data: string = new Date().toISOString();
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.carro = this.navParams.get('carro');
     this.precoTotal = this.navParams.get('precoTotal')
   }
 
+  agenda() {
+    console.log(this.nome);
+    console.log(this.endereco);
+    console.log(this.email);
+    console.log(this.data);
+  }
 }
