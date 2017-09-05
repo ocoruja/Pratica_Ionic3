b8228caeac6418cc327c364aef67219fb5f61a29
ocoruja <tgribeiro@gmail.com>
2017-09-05 18:44:16 -0300

    Reenviando agendamentos

--- src/domain/agendamento/agendamento-service.ts
+++ src/domain/agendamento/agendamento-service.ts
@@ -10,9 +10,14 @@ export class AgendamentoService {
     //constructor(private _http: Http, private _storage: Storage) {}
     constructor(private _http: Http, private _dao: AgendamentoDao) {}
 
-    agenda(agendamento: Agendamento) {
-        let api = `https://aluracar.herokuapp.com/salvarpedido?carro=${agendamento.carro.nome}&preco=${agendamento.valor}&nome=${agendamento.nome}&endereco=${agendamento.endereco}&email=${agendamento.email}&dataAgendamento=${agendamento.data}`;
+    private _montaUri(agendamento: Agendamento){
+        return `https://aluracar.herokuapp.com/salvarpedido?carro=${agendamento.carro.nome}&preco=${agendamento.valor}&nome=${agendamento.nome}&endereco=${agendamento.endereco}&email=${agendamento.email}&dataAgendamento=${agendamento.data}`;
+    }
 
+    agenda(agendamento: Agendamento) {
+
+        let api = this._montaUri(agendamento);
+        
         return this._dao
             .ehAgendamentoDuplicado(agendamento)
             .then(existe => {
@@ -30,5 +35,22 @@ export class AgendamentoService {
                     .then(() => agendamento.confirmado);
             })
 
+    }
+
+    reagenda(agendamento: Agendamento){
+
+        let api = this._montaUri(agendamento);
+        
+        return this._http
+            .get(api)
+            .toPromise()
+            .then(() => agendamento.confirmado = true, err => console.log(err))
+            .then(() => this._dao.salva(agendamento))
+    
+            //.then(() => {
+            //    let key = agendamento.email + agendamento.data.substr(0, 10);
+            //    this._storage.set(key, agendamento);
+            //})
+            .then(() => agendamento.confirmado);
     }
 }
